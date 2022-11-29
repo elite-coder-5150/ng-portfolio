33020fa42c3b65a4ecd74ab25657c6260cd25165
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-29 17:29:09 -0600

    added validation of eamails just needs the regular expression

--- src/app/services/form-validation.service.ts
+++ src/app/services/form-validation.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@angular/core';
+// import { * } as zod from 'zod'
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class FormValidationService {
 
-  constructor() { }
+    constructor() { }
 
-  isRequired = () => {
-    return true;
-    console.log('testing');
-  }
+    isRequired(value: string): boolean | string {
+        if (!value || !value.length) {
+            return "this field is required";
+        }
+
+        return true;
+    }
+
+    email(email: string): boolean | string {
+        if (!email && !email.length) {
+            return true;
+        }
+
+        //? check to see if the format is satisified or not
+        //? if the format is not correct is displays a message
+        //? otherwise return true.
+        let regEx = new RegExp('', '');
+
+        if (!regEx) {
+            return 'error message'
+        }
+
+        return true
+    }
 }
