8d6eca5fc9a6a7d108d0a1f43d76b5140f8a23b1
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-29 17:15:04 -0600

    I added the onSubmit method

--- src/app/newsletter-form/newsletter-form.component.ts
+++ src/app/newsletter-form/newsletter-form.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { NewsletterService } from '../services/newsletter.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Contact } from '../models/contact';
 
 @Component({
-  selector: 'app-newsletter-form',
-  templateUrl: './newsletter-form.component.html',
-  styleUrls: ['./newsletter-form.component.scss']
+    selector: 'app-newsletter-form',
+    templateUrl: './newsletter-form.component.html',
+    styleUrls: ['./newsletter-form.component.scss']
 })
 export class NewsletterFormComponent implements OnInit {
 
-  constructor() { }
+    @Input() contact: Contact = {
+        id: undefined,
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        message: ''
+    }
 
-  ngOnInit(): void {
-  }
+    // @ts-ignore
+    form: FormGroup;
 
+    @Output() save = new EventEmitter<Contact>();
+
+    constructor(private fb: FormBuilder, private ns: NewsletterService) { }
+
+    ngOnInit(): void {
+    }
+
+    onSubmit() {
+        //? use the Zod package for typescript
+        //? for validation
+        if (this.form.valid) {
+            this.save.emit(this.form.value);
+        }
+    }
 }
