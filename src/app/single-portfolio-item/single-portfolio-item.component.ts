a12c0f33b06f84b945c735cca5431fce0c540c3f
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-29 15:30:19 -0600

    logic for the component

---
+++ src/app/single-portfolio-item/single-portfolio-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-portfolio-item',
+  templateUrl: './single-portfolio-item.component.html',
+  styleUrls: ['./single-portfolio-item.component.scss']
+})
+export class SinglePortfolioItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
