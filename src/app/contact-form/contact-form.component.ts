6fa40be116b343631750666da9f650f382109f09
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-29 17:34:01 -0600

    coded the on submit method

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { Contact } from '../models/contact';
 import { ContactService } from '../services/contact.service';
+import { FormValidationService } from '../services/form-validation.service';
 
 @Component({
     selector: 'ng-contact-form',
@@ -9,35 +10,20 @@ import { ContactService } from '../services/contact.service';
     styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-    //? goto angular.io and find out how they create forms.
-    // @ts-ignore
-    formData: FormGroup;
+    firstName = new FormControl('')
+    lastName =  new FormControl('')
+    email = new FormControl('')
+    phoneNumber = new FormControl('');
+    message = new FormControl('');
 
+    constructor(private cs: ContactService,  private fv: FormValidationService) {
 
-    contact: Contact = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        message: '',
-        phoneNumber: ''
     }
-    constructor(private fb: FormBuilder, private cs: ContactService) { }
-
     ngOnInit(): void {
-        //? 
-        this.formData = this.fb.group({
-            firstName: new FormControl('', [Validators.required]),
-            lastName: new FormControl('', [Validators.required]),
-            // @ts-ignore
-            email: new FormControl('', [Validators.compose([Validators.required, Validators.email])])
-        })
+        
     }
 
-    onSubmit(formData: FormGroup) {
-        //? submit the form data to the database.
-        console.log(formData);
-
-        //? validation is 
-        this.cs.sendFormData()
+    onSubmit() {
+        
     }
 }
