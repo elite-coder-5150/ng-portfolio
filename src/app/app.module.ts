8ec82210135bbd1e8aed96077c261f9a21b14434
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-29 12:53:49 -0600

    imported FormsModule and ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PortfolioItemComponent } from './portfolio-item/portfolio-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContactFormComponent,
-    HeaderComponent,
-    FooterComponent,
-    PortfolioItemComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ContactFormComponent,
+        HeaderComponent,
+        FooterComponent,
+        PortfolioItemComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
