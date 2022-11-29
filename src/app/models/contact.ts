297aa56f3fdf17ae579b4c366e02a176c12fab79
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-11-29 13:41:24 -0600

    made all the fields optional

--- src/app/models/contact.ts
+++ src/app/models/contact.ts
@@ -1,7 +1,7 @@
 export interface Contact {
-    firstName: string;
-    lastName: string;
-    email: string;
-    message: string;
-    phoneNumber: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    message?: string;
+    phoneNumber?: string;
 }
