f47230f0c5d4699ac9bcd4f459838f7144d3124b
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-12-02 15:58:07 -0600

    added the default route and component to load when user lands on a url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioItemComponent } from './portfolio-item/portfolio-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PortfolioItemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
