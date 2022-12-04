03ae1d4b5d541b8d54232cc924ff91544364def6
elite-coder-5150 <parkhousedarrell7@gmail.com>
2022-12-03 21:10:44 -0600

    changed the name of the selelctor

--- src/app/portfolio-item/portfolio-item.component.ts
+++ src/app/portfolio-item/portfolio-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-portfolio-item',
+  selector: 'ng-portfolio-item',
   templateUrl: './portfolio-item.component.html',
   styleUrls: ['./portfolio-item.component.scss']
 })
